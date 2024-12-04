27ca3defe45a9e82fbf0fd98a6bae695a3f02ed8
Joseph Onyango <onyango.joseph@yahoo.com>
2024-12-04 12:39:38 +0300

    Seller Registration

--- admin-panel/register.php
+++ admin-panel/register.php
@@ -1,5 +1,44 @@
 <?php 
     include '../components/connect.php';
+
+    if (isset($_POST['submit'])) {
+        $id = unique_id();
+        $name = $_POST['name'];
+        $name = filter_var($name, FILTER_SANITIZE_STRING);
+
+        $email = $_POST['email'];
+        $email = filter_var($email, FILTER_SANITIZE_STRING);    
+
+        $pass = sha1($_POST['pass']);
+        $pass = filter_var($pass, FILTER_SANITIZE_STRING);   
+        
+        $cpass = sha1($_POST['cpass']);
+        $cpass = filter_var($cpass, FILTER_SANITIZE_STRING);  
+
+        $image = $_FILES['image']['name'];
+        $image = filter_var($image, FILTER_SANITIZE_STRING);
+        $ext = pathinfo($image, PATHINFO_EXTENSION);;
+        $rename = unique_id().'.'.$ext;
+        $image_size = $_FILES['image']['size'];
+        $image_tmp_name = $_FILES['image']['tmp_name'];
+        $image_folder = '../uploaded_files/'.$rename;
+
+        $select_seller = $conn->prepare("SELECT * FROM `sellers` WHERE email = ?");
+        $select_seller->execute([$email]);
+
+        if ($select_seller->rowCount() > 0) {
+            $warning_msg[] = 'Email already in use.';
+        } else {
+            if ($pass != $cpass) {
+                $warning_msg[] = 'Passwords not matching.';
+            } else {
+                $insert_seller = $conn->prepare("INSERT INTO `sellers` (id, name, email, password, image) VALUES(?, ?, ?, ?, ?)");
+                $insert_seller->execute([$id, $name, $email, $cpass, $rename]);
+                move_uploaded_file($image_tmp_name, $image_folder);
+                $success_msg[] = 'New seller created successfully.';
+            }
+        }
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,21 +59,21 @@
                 <div class="col">
                     <div class="input-field">
                         <p>Your Name <span>*</span></p>
-                        <input type="text" name="name" placeholder="Enter Your Name" maxLength="5" required class="box" >
+                        <input type="text" name="name" placeholder="Enter Your Name" maxLength="50" required class="box" >
                     </div>
                     <div class="input-field">
                         <p>Your Email <span>*</span></p>
-                        <input type="email" name="email" placeholder="Enter Your Email" maxLength="5" required class="box" >
+                        <input type="email" name="email" placeholder="Enter Your Email" maxLength="50" required class="box" >
                     </div>
                 </div>
                 <div class="col">
                     <div class="input-field">
                         <p>Your Password <span>*</span></p>
-                        <input type="password" name="pass" placeholder="Enter Your Password" maxLength="5" required class="box" >
+                        <input type="password" name="pass" placeholder="Enter Your Password" maxLength="50" required class="box" >
                     </div>
                     <div class="input-field">
                         <p>Confirm Password <span>*</span></p>
-                        <input type="password" name="cpass" placeholder="Confirm Your Password" maxLength="5" required class="box" >
+                        <input type="password" name="cpass" placeholder="Confirm Your Password" maxLength="50" required class="box" >
                     </div>                    
                 </div>                
             </div>
@@ -43,7 +82,7 @@
                 <input type="file" name="image" accept="image/*" required class="box" >
             </div>
             <p class="link">Already have an Account? <a href="login.php">Login Here</a></p>
-            <input type="submit" name="submi" value="Register Now" class="btn" >
+            <input type="submit" name="submit" value="Register Now" class="btn" >
         </form>
     </div>
     
@@ -52,7 +91,7 @@
     <!-- Custom JS Link -->
     <script src="../js/script.js"></script>
     <?php
-        include '../components/alert.php'
+        include '../components/alert.php';
     ?>
 </body>
 </html>
--- components/connect.php
+++ components/connect.php
@@ -15,7 +15,7 @@
 
     function unique_id() {
         $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $charLength = strlen($chars);
+        $charLength = strLen($chars);
         $randomString = '';
         for ($i=0; $i<20; $i++) {
             $randomString.=$chars[mt_rand(0, $charLength - 1)];
