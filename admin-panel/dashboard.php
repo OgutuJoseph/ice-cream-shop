15dc031d346d0996de44fddf76ad065d2e3477e4
Joseph Onyango <onyango.joseph@yahoo.com>
2024-12-05 19:17:43 +0300

    Add Product

--- admin-panel/dashboard.php
+++ admin-panel/dashboard.php
@@ -13,7 +13,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ice Cream Shop - Seller Dashboard</title>
+    <title>Ice Cream Shop - Dashboard</title>
     <link rel="stylesheet" type="text/css" href="../css/admin_style.css" >
     <!-- Font Awesome CDN Link -->
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" >
