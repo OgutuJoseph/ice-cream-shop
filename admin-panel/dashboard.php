7fabf2453e24f014a65165d4ba42f58e27e64c28
Joseph Onyango <onyango.joseph@yahoo.com>
2024-12-05 18:27:14 +0300

    Seller Dashboard

--- admin-panel/dashboard.php
+++ admin-panel/dashboard.php
@@ -41,13 +41,93 @@
                 </div>
                 <div class="box">
                     <?php
-                        $select_message = $conn->prepare("SELECT * FROM `message`");
-                        $select_message->execute();
-                        $number_of_message = $select_message->rowCount();
+                        $select_messages = $conn->prepare("SELECT * FROM `message`");
+                        $select_messages->execute();
+                        $number_of_messages = $select_messages->rowCount();
                     ?>
-                    <h3><?= $number_of_message ?></h3>
+                    <h3><?= $number_of_messages ?></h3>
                     <p>Unread mesages</p>
                     <a href="admin_message.php" class="btn">Messages</a>
+                </div>
+                <div class="box">
+                    <?php
+                        $select_products = $conn->prepare("SELECT * FROM `products` WHERE seller_id = ?");
+                        $select_products->execute([$seller_id]);
+                        $number_of_products = $select_products->rowCount();
+                    ?>
+                    <h3><?= $number_of_products ?></h3>
+                    <p>Products Added</p>
+                    <a href="add_product.php" class="btn">Add Product</a>
+                </div>
+                <div class="box">
+                    <?php
+                        $select_active_products = $conn->prepare("SELECT * FROM `products` WHERE seller_id = ? AND status = ?");
+                        $select_active_products->execute([$seller_id, 'active']);
+                        $number_of_active_products = $select_active_products->rowCount();
+                    ?>
+                    <h3><?= $number_of_active_products ?></h3>
+                    <p>Active Products</p>
+                    <a href="view_products.php" class="btn">Active Products</a>
+                </div>
+                <div class="box">
+                    <?php
+                        $select_inactive_products = $conn->prepare("SELECT * FROM `products` WHERE seller_id = ? AND status = ?");
+                        $select_inactive_products->execute([$seller_id, 'inactive']);
+                        $number_of_inactive_products = $select_inactive_products->rowCount();
+                    ?>
+                    <h3><?= $number_of_active_products ?></h3>
+                    <p>Inactive Products</p>
+                    <a href="view_products.php" class="btn">Inactive Products</a>
+                </div>
+                <div class="box">
+                    <?php
+                        $select_users = $conn->prepare("SELECT * FROM `users`");
+                        $select_users->execute();
+                        $number_of_users = $select_users->rowCount();
+                    ?>
+                    <h3><?= $number_of_users ?></h3>
+                    <p>User Accounts</p>
+                    <a href="user_accounts.php" class="btn">Users</a>
+                </div>
+                <div class="box">
+                    <?php
+                        $select_sellers = $conn->prepare("SELECT * FROM `sellers`");
+                        $select_sellers->execute();
+                        $number_of_sellers = $select_sellers->rowCount();
+                    ?>
+                    <h3><?= $number_of_sellers ?></h3>
+                    <p>Seller Accounts</p>
+                    <a href="user_accounts.php" class="btn">Sellers</a>
+                </div>
+                <div class="box">
+                    <?php
+                        $select_orders = $conn->prepare("SELECT * FROM `orders` WHERE seller_id = ?");
+                        $select_orders->execute([$seller_id]);
+                        $number_of_orders = $select_orders->rowCount();
+                    ?>
+                    <h3><?= $number_of_orders ?></h3>
+                    <p>All Orders Placed</p>
+                    <a href="admin_order.php" class="btn">Orders</a>
+                </div>
+                <div class="box">
+                    <?php
+                        $select_confirmed_orders = $conn->prepare("SELECT * FROM `orders` WHERE seller_id = ? AND status = ?");
+                        $select_confirmed_orders->execute([$seller_id, 'In Progress']);
+                        $number_of_confirmed_orders = $select_confirmed_orders->rowCount();
+                    ?>
+                    <h3><?= $number_of_confirmed_orders ?></h3>
+                    <p>All Confirmed Orders</p>
+                    <a href="admin_order.php" class="btn">Confirmed Orders</a>
+                </div>
+                <div class="box">
+                    <?php
+                        $select_cancelled_orders = $conn->prepare("SELECT * FROM `orders` WHERE seller_id = ? AND status = ?");
+                        $select_cancelled_orders->execute([$seller_id, 'Cancelled']);
+                        $number_of_cancelled_orders = $select_cancelled_orders->rowCount();
+                    ?>
+                    <h3><?= $number_of_cancelled_orders ?></h3>
+                    <p>All Cancelled Orders</p>
+                    <a href="admin_order.php" class="btn">Cancelled Orders</a>
                 </div>
             </div>
         </section>
