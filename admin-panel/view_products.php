923052dec26004bd1f06806c4dbc51da39ff1b9d
Joseph Onyango <onyango.joseph@yahoo.com>
2024-12-10 16:36:06 +0300

    View Products CSS

--- admin-panel/view_products.php
+++ admin-panel/view_products.php
@@ -33,52 +33,54 @@
                 <h1>Your Products</h1>
                 <img src="../image/separator-img.png" >
             </div>
-            <?php
-                $select_products = $conn->prepare("SELECT * FROM `products` WHERE seller_id = ?");
-                $select_products->execute([$seller_id]);
+            <div class="box-container">
+                <?php
+                    $select_products = $conn->prepare("SELECT * FROM `products` WHERE seller_id = ?");
+                    $select_products->execute([$seller_id]);
 
-                if ($select_products->rowCount() > 0) {
-                    while($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)) {
-            ?>
-            <form action="" method="post" class="box">
-                <input type="hidden" name="product_id" value="<?= $fetch_products['id']; ?>";>
-                <?php if($fetch_products['image'] != '') { ?>
-                    <img src="../uploaded_files/<?= $fetch_products['image']; ?>" >
-                <?php } ?>
+                    if ($select_products->rowCount() > 0) {
+                        while($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)) {
+                ?>
+                <form action="" method="post" class="box">
+                    <input type="hidden" name="product_id" value="<?= $fetch_products['id']; ?>";>
+                    <?php if($fetch_products['image'] != '') { ?>
+                        <img src="../uploaded_files/<?= $fetch_products['image']; ?>" class="image">
+                    <?php } ?>
 
-                <div 
-                    class="status" style="color: <?php if($fetch_products['status'] == 'active') {
-                    echo"limegreen";} else {echo "red";} ?>
-                ">
-                    <?= $fetch_products['status']; ?>
-                </div>
-
-                <div class="price"><?= $fetch_products['price']; ?></div>
-
-                <div class="content">
-                    <img src="../image/shape-19.png" class="shap">
-                    <div class="title"><?= $fetch_products['name']; ?></div>
-                    <div class="flex-btn">
-                        <a href="edit_product.php?id=<?= $fetch_products['id']; ?>" class="btn">Edit</a>
-                        <button type="submit" name="delete" class="btn" onclick="return confirm('Are you sure you want to delete this product?');">Delete</button>
-                        <a href="read_product.php?id=<?= $fetch_products['id']; ?>" class="btn">Read Details</a>
+                    <div 
+                        class="status" style="color: <?php if($fetch_products['status'] == 'active') {
+                        echo"limegreen";} else {echo "red";} ?>
+                    ">
+                        <?= $fetch_products['status']; ?>
                     </div>
-                </div>
-            </form>
-            <?php
+
+                    <div class="price">$<?= $fetch_products['price']; ?>/-</div>
+
+                    <div class="content">
+                        <img src="../image/shape-19.png" class="shap">
+                        <div class="title"><?= $fetch_products['name']; ?></div>
+                        <div class="flex-btn">
+                            <a href="edit_product.php?id=<?= $fetch_products['id']; ?>" class="btn">Edit</a>
+                            <button type="submit" name="delete" class="btn" onclick="return confirm('Are you sure you want to delete this product?');">Delete</button>
+                            <a href="read_product.php?id=<?= $fetch_products['id']; ?>" class="btn">Details</a>
+                        </div>
+                    </div>
+                </form>
+                <?php
+                        }
+                    } else {
+                        echo '
+                            <div class="empty">
+                                <p>
+                                    No products available. 
+                                    <br>
+                                    <a href="add_products.php" class="btn" style="margin-top: 1.5rem;">Add Products</a>
+                                </p>
+                            </div> 
+                        ';
                     }
-                } else {
-                    echo '
-                        <div class="empty">
-                            <p>
-                                No products available. 
-                                <br>
-                                <a href="add_products.php" class="btn" style="margin-top: 1.5rem;">Add Products</a>
-                            </p>
-                        </div> 
-                    ';
-                }
-            ?>
+                ?>
+            </div>
         </section>
     </div>      
    
