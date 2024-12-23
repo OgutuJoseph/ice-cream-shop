558be620d220b5fed44a500628a62c0de539b03d
Joseph Onyango <onyango.joseph@yahoo.com>
2024-12-23 14:02:21 +0300

    User Registration

---
+++ register.php
@@ -0,0 +1,127 @@
+<?php 
+    include 'components/connect.php';
+
+    if (isset($_COOKIE['user_id'])) {
+        $user_id = $_COOKIE['user_id'];
+    } else {
+        $user_id = '';
+    }
+
+    if (isset($_POST['submit'])) {
+        $id = unique_id();
+        $name = $_POST['name'];
+        $name = filter_var($name, FILTER_SANITIZE_SPECIAL_CHARS);
+
+        $email = $_POST['email'];
+        $email = filter_var($email, FILTER_SANITIZE_SPECIAL_CHARS);    
+
+        $pass = sha1($_POST['pass']);
+        $pass = filter_var($pass, FILTER_SANITIZE_SPECIAL_CHARS);   
+        
+        $cpass = sha1($_POST['cpass']);
+        $cpass = filter_var($cpass, FILTER_SANITIZE_SPECIAL_CHARS);  
+
+        $image = $_FILES['image']['name'];
+        $image = filter_var($image, FILTER_SANITIZE_SPECIAL_CHARS);
+        $ext = pathinfo($image, PATHINFO_EXTENSION);;
+        $rename = unique_id().'.'.$ext;
+        $image_size = $_FILES['image']['size'];
+        $image_tmp_name = $_FILES['image']['tmp_name'];
+        $image_folder = 'uploaded_files/'.$rename;
+
+        $select_user = $conn->prepare("SELECT * FROM `users` WHERE email = ?");
+        $select_user->execute([$email]);
+
+        if ($select_user->rowCount() > 0) {
+            $warning_msg[] = 'Email already in use.';
+        } else {
+            if ($pass != $cpass) {
+                $warning_msg[] = 'Passwords not matching.';
+            } else {
+                $insert_user = $conn->prepare("INSERT INTO `users` (id, name, email, password, image) VALUES(?, ?, ?, ?, ?)");
+                $insert_user->execute([$id, $name, $email, $cpass, $rename]);
+                move_uploaded_file($image_tmp_name, $image_folder);
+                $success_msg[] = 'New user created successfully.';
+            }
+        }
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ice Cream Shop | User Registration</title>
+    <link rel="stylesheet" type="text/css" href="css/user_style.css" >
+    <!-- Font Awesome CDN Link -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" >
+     <!-- Box Icon CDN Link -->
+     <!-- <link href="https://cdn.jsdelivr.net/npm/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet"> -->
+     <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" >
+</head>
+<body>
+    <?php
+        include 'components/user_header.php';
+    ?>
+    <!-- Banner Section Start -->
+    <div class="banner">
+        <div class="detail">
+            <h1>Register</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur enim erat, vehicula vel vestibulum at, egestas interdum ante. <br>
+            In luctus non justo ultricies porttitor. Etiam vehicula blandit accumsan.</p>
+            <span><a href="home.php">Home</a><i class="bx bx-right-arrow-alt"></i>Register</span>
+        </div>
+    </div>
+    <!-- Banner Section End -->
+
+    <div class="form-container">
+        <form action="" method="post" enctype="multipart/form-data" class="register">
+            <h3>Register Now</h3>
+            <div class="flex">
+                <div class="col">
+                    <div class="input-field">
+                        <p>Your Name <span>*</span></p>
+                        <input type="text" name="name" placeholder="Enter Your Name" maxLength="50" required class="box" >
+                    </div>
+                    <div class="input-field">
+                        <p>Your Email <span>*</span></p>
+                        <input type="email" name="email" placeholder="Enter Your Email" maxLength="50" required class="box" >
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="input-field">
+                        <p>Your Password <span>*</span></p>
+                        <input type="password" name="pass" placeholder="Enter Your Password" maxLength="50" required class="box" >
+                    </div>
+                    <div class="input-field">
+                        <p>Confirm Password <span>*</span></p>
+                        <input type="password" name="cpass" placeholder="Confirm Your Password" maxLength="50" required class="box" >
+                    </div>                    
+                </div>                
+            </div>
+            <div class="input-field">
+                <p>Your Photo <span>*</span></p>
+                <input type="file" name="image" accept="image/*" required class="box" >
+            </div>
+            <p class="link">Already have an Account? <a href="login.php">Login Here</a></p>
+            <input type="submit" name="submit" value="Register Now" class="btn" >
+        </form>
+    </div>
+
+ 
+
+
+    
+    <?php
+        include 'components/user_footer.php';
+    ?>
+
+    <!-- Sweet Alert CDN Link -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+    <!-- Custom JS Link -->
+    <script src="js/user_script.js"></script>
+    <?php
+        include 'components/alert.php';
+    ?>
+</body>
+</html>
